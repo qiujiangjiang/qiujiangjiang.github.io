4d18a3b14e1b306602e78bdb8863f2ebac8a02c4
qiujiangjiang <1157060413@qq.com>
2024-11-25 00:40:38 +0800

    Commit by qjj

--- updata.sh
+++ updata.sh
@@ -4,4 +4,4 @@
 # git push -f https://github.com/qiujiangjiang/qiujiangjiang.github.io.git
 git add .
 git commit -m "Commit by $(whoami)"
-git push https://gitee.com/qiujiangjiang/blog-source
+git push https://gitee.com/qiujiangjiang/blog-source master
